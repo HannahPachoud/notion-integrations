d928e8a97dd2f925253342c41bbe8a34e0d39d03
Hannah Pachoud <hannah.pachoud@xero.com>
2023-11-06 17:42:48 +1300

    first version charts working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
 import React, { Component, useEffect, useState } from "react";
 import logo from "./logo.svg";
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+  PieChart,
+  Pie,
+  ResponsiveContainer,
+} from "recharts";
+import { ResponsiveBar } from "@nivo/bar";
+import { ResponsivePie } from "@nivo/pie";
 import "./App.css";
 import Graph from "./components/Graph";
 
@@ -17,20 +31,41 @@ export default function App() {
 
   const categorise = (expenses) =>
     expenses.reduce((acc, item) => {
-      console.log(acc, item);
-      acc = {
-        ...acc,
-        [item.category]: (acc[item.category] || 0) + item.amount,
-      };
+      const prevValue = acc.get(item.category) || 0;
+      acc.set(item.category, prevValue + item.amount)
       return acc;
-    }, {});
+    }, new Map());
+
+  const data = [...categorise(expenses)]
+    .map(([category, amount]) => ({category, amount}));
+  console.log(data)
 
   return (
-    <div className="App">
+    <div className="App" style={{ height: 400 }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {/* <p className="App-intro">{expenses)}</p> */}
-        <Graph categories={categorise(expenses)}></Graph>
+        {/* <Graph categories={categorise(expenses)}></Graph> */}
+        <BarChart width={730} height={250} data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="category" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="amount" fill="#8884d8" />
+        </BarChart>
+          <PieChart width={730} height={250}>
+            <Pie
+              data={data}
+              dataKey="amount"
+              nameKey="category"
+              cx="50%"
+              cy="50%"
+              outerRadius={50}
+              fill="#8884d8"
+            />
+            <Tooltip />
+          </PieChart>
         <a
           className="App-link"
           href="https://reactjs.org"
