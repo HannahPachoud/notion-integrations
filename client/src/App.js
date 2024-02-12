ddb992f086577fed83d51a9e13cb9675ca4593f4
Hannah Pachoud <hannah.pachoud@gmail.com>
2024-02-12 14:25:22 +1300

    move graph into its own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,44 @@
-import React, { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import "./App.css";
-import {ResponsiveBar} from "@nivo/bar";
+import Graph from "./components/Graph";
 
 export default function App() {
-  const barColors = ["#1f77b4", "#ff7f0e", "#2ca02c"];
+    const barColors = ["#1f77b4", "#ff7f0e", "#2ca02c"];
 
-  const [expenses, setExpenses] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const searchParams = new URLSearchParams(window.location.search);
+    const [expenses, setExpenses] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const searchParams = new URLSearchParams(window.location.search);
 
-  const callNotionAPI = () =>
-    fetch(`http://localhost:9000/notionDB${window.location.search}`)
-      .then((res) => res.text())
-      .then((res) => setExpenses(JSON.parse(res)));
+    const callNotionAPI = () =>
+        fetch(`http://localhost:9000/notionDB${window.location.search}`)
+            .then((res) => res.text())
+            .then((res) => setExpenses(JSON.parse(res)));
 
-  const getNotionCategories = () =>
-    fetch("http://localhost:9000/notionDB/categories")
-      .then((res) => res.text())
-      .then((res) => setCategories(JSON.parse(res)));
+    const getNotionCategories = () =>
+        fetch("http://localhost:9000/notionDB/categories")
+            .then((res) => res.text())
+            .then((res) => setCategories(JSON.parse(res)));
 
-  useEffect(() => {
-    callNotionAPI();
-    getNotionCategories();
-  }, []);
+    useEffect(() => {
+        callNotionAPI();
+        getNotionCategories();
+    }, []);
 
-  const categorise = (expenses) =>
-    expenses.reduce((acc, item) => {
-      const prevValue = acc.get(item.category) || 0;
-      acc.set(item.category, prevValue + item.amount);
-      return acc;
-    }, new Map());
+    const categorise = (expenses) =>
+        expenses.reduce((acc, item) => {
+            const prevValue = acc.get(item.category) || 0;
+            acc.set(item.category, prevValue + item.amount);
+            return acc;
+        }, new Map());
 
-  const data = [...categorise(expenses)].map(([category, amount]) => ({
-    category: categories.find((x) => x.id === category)?.name,
-    amount
-  }));
+    const data = [...categorise(expenses)].map(([category, amount]) => ({
+        category: categories.find((x) => x.id === category)?.name,
+        amount
+    }));
 
-  const barColor = "#bd7a0f";
-  console.log(data)
-    const amounts = data.filter(([category, amount]) => ({amount}));
-    // const max = Math.max(amounts)
-    console.log(amounts);
+    const barColor = "#bd7a0f";
+    console.log(data)
+    const graph = Graph(data);
 
-  return (
-    <div className="App" style={{ height: 300 }}>
-      <ResponsiveBar
-          width={800}
-          height={300}
-          data={data}
-          keys={["amount"]}
-          maxValue={2500}
-          padding={0.6}
-          margin={{
-            top: 50,
-            right: 10,
-            bottom: 36,
-            left: 36
-          }}
-          indexBy="category"
-          enableLabel={false}
-          colors={{scheme: 'purpleRed_green'}}
-          colorBy={"index"}
-          borderRadius={2}
-          axisLeft={{
-            tickValues: 7
-          }}
-      />
-    </div>
-  );
+    return (graph);
 }
